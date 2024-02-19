618b52e2dfa56f36c98bd1e124ab867d1e2b7e76
DorantVelija <dorantvelija@outlook.com>
2024-02-19 18:13:40 +0100

    fixed whyus part

--- euroline/src/WhyUs.js
+++ euroline/src/WhyUs.js
@@ -2,14 +2,27 @@ const WhyUs = () => {
     return ( 
         <div className="whyUs">
             <div className="left">
+               
                 <h1>Convenient and Hassle-Free</h1>
+                <p>Our rental car service provides a seamless solution for short-term transportation needs, ensuring a hassle-free experience for customers.</p>
+               
                 <h1>Diverse Fleet</h1>
+                <p>Choose from a diverse fleet of well-maintained vehicles, ranging from compact cars to spacious SUVs, catering to various preferences and requirements.</p>
+               
                 <h1>User-Friendly Online Platform</h1>
+               
+                <p>Our intuitive online platform simplifies the booking process, allowing customers to easily select and reserve their preferred vehicle with just a few clicks.</p>
             </div>
             <div className="right">
+                
                 <h1>Competetive Pricing</h1>
+                <p>Benefit from competitive pricing, ensuring that customers receive value for their money when securing reliable and comfortable transportation.</p>
+                
                 <h1>Flexible Rental Options</h1>
+                <p>With flexible rental options, our service accommodates the dynamic needs of customers, whether for business trips, vacations, or daily commuting.</p>
+                
                 <h1>Transparent Terms</h1>
+                <p>Clear and transparent terms provide customers with confidence, making it straightforward to understand and adhere to the conditions of their rental agreement.</p>
             </div>
         </div>
      );
