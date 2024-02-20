3eed7bf3448c1a01644de9eecccddd7b625e9aab
DorantVelija <dorantvelija@outlook.com>
2024-02-20 04:16:48 +0100

    updated carblocks to no outline

--- euroline/src/OurFleet.js
+++ euroline/src/OurFleet.js
@@ -6,7 +6,7 @@ import audi from './audi.png';
 const OurFleet = () => {
     return (  
         <div className="fleet">
-            <h1>OUR FLEET</h1>
+            <h1 id="titleFleet">OUR FLEET</h1>
             <div className="carBlocks">
                 <CarBlock carName="VW GOLF 7" carPrice="75" carImg={carRed}/>
                 <CarBlock carName="VW PASSAT" carPrice="80" carImg={passat}/>
