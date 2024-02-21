da1171563f9d02f1d9e552263565a6619b990b51
DorantVelija <dorantvelija@outlook.com>
2024-02-21 05:45:54 +0100

    even more animations!

--- euroline/src/App.js
+++ euroline/src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   const scrollToOptions = () => {
     if (fleetRef.current) {
-        fleetRef.current.scrollIntoView({ behavior: 'smooth' });
+        fleetRef.current.scrollIntoView({ behavior: 'smooth'});
     } 
   };
 
