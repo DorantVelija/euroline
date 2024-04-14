69382a521636008ae151ce4f02bc3762e5c5ea39
DorantVelija <dorantvelija@outlook.com>
2024-04-14 15:20:36 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './navbar';
+import Home from './Home';
+import OurFleet from './OurFleet';
+import WhyUs from './WhyUs';
+import Footer from './footer';
+import React, { useRef, useEffect } from 'react';
 
 function App() {
+
+  const fleetRef = useRef(null);
+
+  const scrollToOptions = () => {
+    if (fleetRef.current) {
+        fleetRef.current.scrollIntoView({ behavior: 'smooth'});
+    } 
+  };
+
+  const handleMouseEnter = (id) => {
+    const carElement = document.getElementById(id);
+    if (carElement !== document.getElementById('mainCar')) {
+      carElement.classList.add('hovered');
+      document.getElementById('mainCar').classList.remove('hovered');
+    }
+  };
+
+  const handleMouseLeave = () => {
+    document.getElementById('car1').classList.remove('hovered');
+    document.getElementById('car2').classList.remove('hovered');
+    document.getElementById('mainCar').classList.add('hovered');
+  };
+ 
+  /*
+  document.getElementById('car1').addEventListener('mouseover', handleMouseEnter('car1'));
+  document.getElementById('car1').addEventListener('mouseleave', handleMouseEnter('car1'));
+  document.getElementById('car2').addEventListener('mouseover', handleMouseEnter('car2'));
+  document.getElementById('car2').addEventListener('mouseleave', handleMouseEnter('car2'));
+*/
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Navbar />
+      <div className="content">
+        <Home scrollToOptions={scrollToOptions}/>
+        <OurFleet innerRef={fleetRef}/>
+        <WhyUs />
+      </div>
+      <Footer />
     </div>
   );
+
 }
 
 export default App;
