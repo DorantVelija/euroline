06733bf122dbe1e4ac70caa1a17ea4dc2e202ef5
DorantVelija <dorantvelija@outlook.com>
2024-07-24 19:50:51 +0200

    fixed padding and screen height for mobile

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -9,8 +9,8 @@ const Landing = () => {
         element.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
     return(
-        <div className="max-md:h-dvh bg-gradient-to-b from-gray-900 to-gray-400">
-            <div className="mx-auto md:max-w-7xl max-md:py-32">
+        <div className="max-md:h-svh bg-gradient-to-b from-gray-900 to-gray-400">
+            <div className="mx-auto md:max-w-7xl max-md:py-24">
                     <Heading color="white" weight="black" size="landing" classes="text-center italic max-md:leading-[72px]">NEED A <span  className="text-blue-500">CAR?</span></Heading>
                     <img src="/assets/car-img-landing.png" alt="" className="md:-translate-y-52 md:max-w-full" draggable="false"/>
                 <div className="flex justify-center md:-translate-y-80">
