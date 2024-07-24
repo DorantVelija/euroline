fe4b00f2cf8f3c09b222995d4f9faaf576759c48
DorantVelija <dorantvelija@outlook.com>
2024-07-24 21:08:09 +0200

    added carousel component lib

--- src/pages/CarPage.js
+++ src/pages/CarPage.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import Button from '../components/Button'
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 import Car1 from '../assets/car-img-1.png';
 import Car2 from "../assets/car-img-2.avif";
@@ -15,7 +17,6 @@ const CarPage = () => {
     const [url, setUrl] = useState('');
 
     useEffect(() => {
-        // Replace 'https://api.example.com/data' with your API endpoint
         fetch('https://picsum.photos/400/')
             .then(response => response.json())
             .then(data => setUrl(data.url));
@@ -23,12 +24,22 @@ const CarPage = () => {
 
     return (
 
-        <section className="text-gray-600 body-font overflow-hidden">
-            <div className="container px-5 pt-10 md:pt-32 pb-72 mx-auto">
+        <section className="container mx-auto text-gray-600 body-font overflow-hidden">
+            <div className="pt-10 md:pt-32 pb-72 mx-auto">
                 <div className="lg:w-4/5 mx-auto flex flex-wrap">
-                    <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-                          src="https://picsum.photos/400/" draggable="false"/>
-                    <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+                    <Carousel showArrows={true} showThumbs={false} className="max-w-[420px] mx-2">
+                        <div>
+                            <img alt="ecommerce"
+                                 className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+                                 src="https://picsum.photos/400/" draggable="false"/>
+                        </div>
+                        <div>
+                            <img alt="ecommerce"
+                                 className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+                                 src="https://picsum.photos/400/" draggable="false"/>
+                        </div>
+                    </Carousel>
+                    <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0 mx-2">
                         <h2 className="text-sm title-font text-gray-500 tracking-widest uppercase">{brand}</h2>
                         <h1 className="text-gray-900 text-3xl title-font font-medium mb-1 capitalize">{name}</h1>
                         <div className="flex mb-4">
@@ -38,7 +49,7 @@ const CarPage = () => {
                             Gearbox: Automatic<br/>
                             Engine: 104HP <br/>
                             Fuel : 3.2L - 7.1L/100km.
-                            </p>
+                        </p>
                         <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                             <div className="flex flex-col">
                                 <div>
